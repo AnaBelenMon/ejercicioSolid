65e4dd2e1e36daf940f85e5eb9ce7666c6f3e4d4
Ana Belén <anabelenmontilla1975@gmail.com>
2026-03-20 19:26:47 +0100

    Corrección de la clase App

--- src/main/java/matricula/App.java
+++ src/main/java/matricula/App.java
@@ -1,11 +1,19 @@
 package matricula;
 
 public class App {
-
     public static void main(String[] args) {
-        MatriculaService servicio = new MatriculaService();
+
+        MatriculaService servicio = new MatriculaService(
+                new validarDni(),
+                new calcularPrecioMatricula(),
+                new repositorioMatriculasEnMemoria(),
+                new notificadorConsola(),
+                new exportadorMatriculaTexto()
+        );
+
         servicio.crearMatricula("12345678A", "DAM1", 300, true);
         servicio.crearMatricula("87654321B", "SMR2", 200, false);
+
         System.out.println(servicio.exportarResumen());
     }
 }
