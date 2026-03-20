f8441dd2aae621175374c61ff582431f26e669cc
Ana Belén <anabelenmontilla1975@gmail.com>
2026-03-20 19:18:51 +0100

    Corrección de la clase BibliotecaApp

--- src/main/java/biblioteca/BibliotecaApp.java
+++ src/main/java/biblioteca/BibliotecaApp.java
@@ -3,22 +3,22 @@ package biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
+
         PrestamoService service = new PrestamoService();
 
-        Libro[] libros = new Libro[2];
-        libros[0] = new Libro("Dune");
-        libros[1] = new LibroReferencia("Diccionario RAE");
+        Libro libro1 = new Libro("Dune");
+        LibroReferencia ref = new LibroReferencia("Diccionario RAE");
 
         // PRESTAR
-        for (Libro libro : libros) {
-            service.prestar(libro, "12345678A");
-        }
+        service.prestar(libro1, "12345678A");
+
+        // Intento de prestar referencia → NO permitido
+        System.out.println("\nIntentando prestar libro de referencia:");
+        System.out.println(ref.getTitulo() + " NO se puede prestar.");
 
         System.out.println();
 
         // DEVOLVER
-        for (Libro libro : libros) {
-            System.out.println("Devuelto (" + libro.getTitulo() + "): " + service.devolver(libro));
-        }
+        System.out.println("Devuelto (" + libro1.getTitulo() + "): " + service.devolver(libro1));
     }
 }
