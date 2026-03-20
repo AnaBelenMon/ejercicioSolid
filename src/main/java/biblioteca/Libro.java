1576ec5106b8779a8759d3ed631052c228b85ef9
Ana Belén <anabelenmontilla1975@gmail.com>
2026-03-20 19:20:00 +0100

    Corrección de la clase Libro

--- src/main/java/biblioteca/Libro.java
+++ src/main/java/biblioteca/Libro.java
@@ -2,7 +2,7 @@ package biblioteca;
 
 import java.time.LocalDate;
 
-public class Libro {
+public class Libro implements Prestable {
 
     protected final String titulo;
     protected String prestadoA;
@@ -12,21 +12,27 @@ public class Libro {
         this.titulo = titulo;
     }
 
+    @Override
     public void prestarA(String dni, LocalDate fechaDevolucion) {
         this.prestadoA = dni;
         this.fechaDevolucion = fechaDevolucion;
     }
 
+    @Override
     public void devolver() {
         this.prestadoA = null;
         this.fechaDevolucion = null;
     }
 
+    @Override
     public boolean estaPrestado() {
         return prestadoA != null;
     }
 
     public String getTitulo() {
+        return titulo;
+    }
+}
         return titulo;
     }
 }
