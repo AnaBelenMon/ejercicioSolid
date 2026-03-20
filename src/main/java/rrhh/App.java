fe2b8ebf78450a5a7d3d5887da1fa8494f245782
Amane <programacionalcazarmartin@gmail.com>
2026-03-21 00:23:38 +0800

    Refactor App to use dependency injection for services

--- src/main/java/rrhh/App.java
+++ src/main/java/rrhh/App.java
@@ -1,9 +1,13 @@
 package rrhh;
 
 public class App {
-
     public static void main(String[] args) {
-        AltaEmpleadoService s = new AltaEmpleadoService();
+
+        RepositorioEmpleados repo = new RepositorioEmpleadosEnMemoria();
+        GenerarPassword generador = new GeneradorPasswordSimple();
+
+        AltaEmpleadoService s = new AltaEmpleadoService(repo, generador);
+
         s.alta("11111111A", "Lucía");
         s.alta("22222222B", "Álvaro");
     }
