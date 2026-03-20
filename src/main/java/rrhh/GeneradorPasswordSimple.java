ed19d8ccd6f8edf2e6e37f4579387a8faadd6cd4
Amane <programacionalcazarmartin@gmail.com>
2026-03-21 00:23:59 +0800

    Implement GenerarPassword interface in GeneradorPasswordSimple

--- src/main/java/rrhh/GeneradorPasswordSimple.java
+++ src/main/java/rrhh/GeneradorPasswordSimple.java
@@ -1,9 +1,8 @@
 package rrhh;
-
 import java.util.UUID;
 
-public class GeneradorPasswordSimple {
-
+public class GeneradorPasswordSimple implements GenerarPassword {
+    @Override
     public String generar() {
         return UUID.randomUUID().toString().substring(0, 8);
     }
