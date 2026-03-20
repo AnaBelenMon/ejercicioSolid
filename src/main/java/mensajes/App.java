a2cf3152d3a3710384e4760f3af90276331bbcf4
Amane <programacionalcazarmartin@gmail.com>
2026-03-20 13:22:43 +0100

    Refactor Mensaje instantiation to use subclasses

--- src/main/java/mensajes/App.java
+++ src/main/java/mensajes/App.java
@@ -5,25 +5,20 @@ public class App {
     public static void main(String[] args) {
         GeneradorTextoMensajes generador = new GeneradorTextoMensajes();
 
-        Mensaje aviso = new Mensaje(
-                TipoMensaje.AVISO,
+        Mensaje aviso = new Aviso(
+
                 "intranet",
                 "Grupo SMR1",
                 "Cambio de aula",
                 "Hoy la clase será en el aula 2.3 por mantenimiento.",
-                "2026-02-26",
-                null,
-                null,
-                null
+                "2026-02-26"
+
         );
 
-        Mensaje incidencia = new Mensaje(
-                TipoMensaje.INCIDENCIA,
+        Mensaje incidencia = new Incidencia(
                 "correo",
-                "Tutor/a",
                 "Retraso reiterado",
                 "Se han detectado 3 retrasos esta semana.",
-                "2026-02-26",
                 "12345678A",
                 "DAM1",
                 "ALTA"
